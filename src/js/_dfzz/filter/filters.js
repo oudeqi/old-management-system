c7b79b4bd23ec6eaa23bc230f8e3ff32d901fc9c
oudeqi <496997374@qq.com>
2016-12-23 21:33:03 +0800

    task put

--- src/js/_dfzz/filter/filters.js
+++ src/js/_dfzz/filter/filters.js
@@ -3,4 +3,10 @@ angular.module('uoudo.dfzz')
     return function(input,i){
         return Math.floor(input*Math.pow(10,i)) / Math.pow(10,i);
     };
+})
+.filter('formatFloat',function(){//去除js计算误差
+    return function(input){
+        var m = Math.pow(10, 1);
+        return parseInt(input * m, 10) / m;
+    };
 });
